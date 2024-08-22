3f727d8c2bc70976fc1a31e5942aab2b7f0ecdc4
Ismail Rohaga <azh.zhafir@gmail.com>
2024-08-22 22:42:41 +0700

    feat: implement iteration and sample var declaration

--- golang/iteration/repeat.go
+++ golang/iteration/repeat.go
@@ -1,5 +1,30 @@
 package iteration
 
+import "fmt"
+
 func Repeat(s string) string {
-	return "ssss"
+	var repeated string
+	for i := 0; i < 4; i++ {
+		repeated = repeated + s
+	}
+	return repeated
+}
+
+// ways to declare variables in go
+func declareVariables() {
+    var a = "initial" // a is a string
+    fmt.Println(a)
+
+    //can declare multiple variables at once.
+    var b, c int = 1, 2 // b and c is an int
+    fmt.Println(b, c)
+
+    var d = true // d is a bool
+    fmt.Println(d)
+
+    var e int 
+    fmt.Println(e) // will print 0 because e is not initialized, thus it's "zero-valued"
+
+    f := "apple" // shorthand declaration for var f string = "apple"
+    fmt.Println(f)
 }
